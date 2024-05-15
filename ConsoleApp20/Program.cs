72dd8be53b273664cfc121684d7f55aed1ed778d
AniCheeze <daniil.avd06@mail.ru>
2024-05-15 19:36:54 +0700

    Система боя

--- ConsoleApp20/Program.cs
+++ ConsoleApp20/Program.cs
@@ -1,19 +1,26 @@
 ﻿using System;
-
-class Program
+partial class Program
 {
     public static void Main(string[] args)
     {
         int menu = 1;
         int menu1 = 1;
         int money = 0;
+        bool lostgame = false;
+        Zombie zombie = new Zombie();
+        Skeleton skeleton = new Skeleton();
+        List<Enemy> list = new List<Enemy>();
+        list.Add(zombie);
+        list.Add(skeleton);
+        list.Add(zombie);
+        Ogre ogre = new Ogre();
         Player P1 = new Player();
         string[] Nmaps = { "Холл", "Техническое помещение", "Буфет", "Комната" };
         string[] maps = { "|---&&---| |********| |********| #********# |********| |********| |********| #********# |***G****| ----&&----", " _________________ |O***?******HHHH| |TTTTTTTTT******| |***********?***| |*T*****T**?**T*| |***?***********| |*T*****T*****T*| |**********?****| |*T**?**T*****T*| |***************| |*T***?*T****GT*| ------------&----", " ----- |**O| |***| |?**| |***| |**G| ---&-", " _______ |H*****H| |*******| |*******| |*******| |***?***| |*******| |***G***| ---&---" };
         string[] Gmap = new string[150];
         while (menu != 0)
         {
-            Console.WriteLine("1 - Начало игры, 2 - прокачка, 3 - информация");
+            Console.WriteLine("1 - Начало игры, 2 - прокачка, 3 - информация, 4 - тест боя");
             try
             {
                 menu = Convert.ToInt32(Console.ReadLine());
@@ -51,6 +58,9 @@ class Program
 
                         break;
                     }
+                case 4:
+                    P1.BattleInitiate(1, list, lostgame);
+                    break;
                 default:
                     {
                         Console.WriteLine("Неверный ввод");
@@ -81,6 +91,26 @@ class Enemy
         hp = 100;
         damage = 10;
     }
+    public void GetDamage(int a)
+    {
+        hp -= a;
+    }
+    public int ReturnDamage()
+    {
+        return damage;
+    }
+    public int ReturnHp()
+    {
+        return hp;
+    }
+    public void SetHp(int a)
+    {
+        hp = a;
+    }
+    public string ReturnName()
+    {
+        return name;
+    }
 }
 class Zombie : Enemy
 {
@@ -88,7 +118,7 @@ class Zombie : Enemy
     {
         name = "Zombie";
         hp = 100;
-        damage = 10;
+        damage = 12;
     }
 }
 class Skeleton : Enemy
@@ -188,14 +218,19 @@ class Player
     int defense;
     //Если будет время
     int fortune;
-
+    bool BO = false;
+    Random rand = new Random();
     public Player()
     {
         name = "Player";
         hp = 100;
-        damage = 10;
+        damage = 15;
         defense = 10;
         fortune = 10;
+    }
+    public int ReturnDamage()
+    {
+        return damage;
     }
     public void Upgrade(int a, int money)
     {
@@ -245,6 +280,97 @@ class Player
                     Console.WriteLine("Ты бедность");
                 }
                 break;
+        }
+    }
+    public void GetDamage(int a)
+    {
+        hp -= (a - defense);
+    }
+    public int ReturnHp()
+    {
+        return hp;
+    }
+    public void SetHp(int a)
+    {
+        hp = a;
+    }
+    public void BattleInitiate(double modifier, List<Enemy> A, bool LG)
+    {
+        int BD;
+        if(BO != true)
+        {
+            BD = rand.Next(2);
+            switch (BD)
+            {
+                case 0:
+                    while(BO != true)
+                    {
+                        Console.WriteLine($"Ваш противник - {A[BD].ReturnName}");
+                        A[BD].GetDamage(damage);
+                        Console.WriteLine($"Вы нанесли зомби {damage} ед урона");
+                        GetDamage(A[BD].ReturnDamage());
+                        Console.WriteLine($"Вы получили {A[BD].ReturnDamage() - defense} ");
+                        if(A[BD].ReturnHp() <= 100)
+                        {
+                            Console.WriteLine("Вы победили");
+                            BO = true;
+                            A[BD].SetHp(100);
+                        }
+                        else if(ReturnHp() <= 100)
+                        {
+                            Console.WriteLine("Вы проиграли");
+                            BO = true;
+                            LG = true;
+                            A[BD].SetHp(100);
+                        }
+                    }
+                    break;
+
+                case 1:
+                    Console.WriteLine($"Ваш противник - {A[BD].ReturnName}");
+                    A[BD].GetDamage(damage);
+                    Console.WriteLine($"Вы нанесли зомби {damage} ед урона");
+                    GetDamage(A[BD].ReturnDamage());
+                    Console.WriteLine($"Вы получили {A[BD].ReturnDamage() - defense} ");
+                    if (A[BD].ReturnHp() <= 100)
+                    {
+                        Console.WriteLine("Вы победили");
+                        BO = true;
+                        A[BD].SetHp(100);
+                    }
+                    else if (ReturnHp() <= 100)
+                    {
+                        Console.WriteLine("Вы проиграли");
+                        BO = true;
+                        LG = true;
+                        A[BD].SetHp(100);
+                    }
+                    break;
+
+                case 2:
+                    Console.WriteLine($"Ваш противник - {A[BD].ReturnName}");
+                    A[BD].GetDamage(damage);
+                    Console.WriteLine($"Вы нанесли зомби {damage} ед урона");
+                    GetDamage(A[BD].ReturnDamage());
+                    Console.WriteLine($"Вы получили {A[BD].ReturnDamage() - defense} ");
+                    if (A[BD].ReturnHp() <= 100)
+                    {
+                        Console.WriteLine("Вы победили");
+                        BO = true;
+                        A[BD].SetHp(100);
+                    }
+                    else if (ReturnHp() <= 100)
+                    {
+                        Console.WriteLine("Вы проиграли");
+                        BO = true;
+                        LG = true;
+                        A[BD].SetHp(100);
+                    }
+                    break;
+                default:
+                        Console.WriteLine("Враг не найден");
+                        break;
+            }
         }
     }
 }
